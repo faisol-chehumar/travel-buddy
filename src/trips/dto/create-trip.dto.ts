805318c787927d11c1bc5bb51206a70029aecba2
Faisol Chehumar <faisol.chehumar@gmail.com>
2025-05-10 01:07:40 +0700

    Create initial Trips module structure with CRUD functionality

---
+++ src/trips/dto/create-trip.dto.ts
@@ -0,0 +1,75 @@
+import { IsString, IsOptional, IsDate, IsNumber, ValidateNested, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class CoordinatesDto {
+  @IsNumber()
+  @IsNotEmpty()
+  latitude: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  longitude: number;
+}
+
+class LocationDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ValidateNested()
+  @Type(() => CoordinatesDto)
+  coordinates: CoordinatesDto;
+}
+
+class StopDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ValidateNested()
+  @Type(() => CoordinatesDto)
+  coordinates: CoordinatesDto;
+
+  @IsNumber()
+  @IsOptional()
+  duration?: number; // in minutes
+}
+
+export class CreateTripDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ValidateNested()
+  @Type(() => LocationDto)
+  startLocation: LocationDto;
+
+  @ValidateNested()
+  @Type(() => LocationDto)
+  endLocation: LocationDto;
+
+  @ValidateNested({ each: true })
+  @Type(() => StopDto)
+  @IsOptional()
+  stops?: StopDto[];
+
+  @IsDate()
+  @Type(() => Date)
+  date: Date;
+
+  @IsNumber()
+  @IsOptional()
+  duration?: number; // in minutes
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+}
